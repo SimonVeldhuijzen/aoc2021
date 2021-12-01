0c610e7d9e174518260aa912c6c809e1235bfbc8
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2021-12-01 10:33:12 +0100

    day 1

--- app/src/main/kotlin/aoc2021/solvers/Day01.kt
+++ app/src/main/kotlin/aoc2021/solvers/Day01.kt
@@ -10,13 +10,13 @@ class Day01 : Solver {
 
     override fun partB(): String {
         val input = InputLoader().loadLongs(1)
-        val windowed = createSlidingSums(input)
+        val windowed = createSlidingSums(input, 3)
         return countIncreases(windowed)
     }
 
     private fun countIncreases(input: List<Long>) =
         input.indices.drop(1).count { input[it] > input[it - 1] }.toString()
 
-    private fun createSlidingSums(input: List<Long>) =
-        input.indices.drop(2).map { input[it] + input[it - 1] + input[it - 2] }
+    private fun createSlidingSums(input: List<Long>, size: Int) =
+        input.indices.drop(size - 1).map { input.drop(it - size + 1).take(size).sum() }
 }
