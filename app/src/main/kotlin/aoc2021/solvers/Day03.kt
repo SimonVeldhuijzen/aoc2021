9cd18ea4bd05f01ac54222540ea4528b69464364
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2021-12-03 08:15:46 +0100

    day 3

--- app/src/main/kotlin/aoc2021/solvers/Day03.kt
+++ app/src/main/kotlin/aoc2021/solvers/Day03.kt
@@ -4,36 +4,42 @@ import aoc2021.helpers.InputLoader
 
 class Day03 : Solver {
     override fun partA(): String {
-        val input = InputLoader().load(2)
-        return input.fold(Position()) { current, next ->
-            val (operation, amount) = next.split(" ")
-            when (operation) {
-                "forward" -> current.mutateHorizontal(amount.toInt())
-                "down" -> current.mutateDepth(amount.toInt())
-                "up" -> current.mutateDepth(amount.toInt() * -1)
-                else -> current
-            }
-        }.multiplication
+        val input = InputLoader().load(3)
+        val halfCount = input.size / 2
+        val gamma = (0 until input[0].length).map { i ->
+            if (input.count { it[i] == '1' } > halfCount) '1' else '0'
+        }.toCharArray().concatToString()
+        val epsilon = gamma.map { if (it == '1') '0' else '1' }.toCharArray().concatToString()
+
+        return (gamma.toInt(2) * epsilon.toInt(2)).toString()
     }
 
     override fun partB(): String {
-        val input = InputLoader().load(2)
+        val input = InputLoader().load(3)
+        val oxygens = input.toMutableList()
+        val co2s = input.toMutableList()
+        for (i in 0 until input[0].length) {
+            filterRatings(oxygens, i, false)
+            filterRatings(co2s, i, true)
+        }
 
-        return input.fold(Position()) { current, next ->
-            val (operation, amount) = next.split(" ")
-            when (operation) {
-                "forward" -> current.mutateDepth(amount.toInt() * current.aim).mutateHorizontal(amount.toInt())
-                "down" -> current.mutateAim(amount.toInt())
-                "up" -> current.mutateAim(amount.toInt() * -1)
-                else -> current
-            }
-        }.multiplication
+        return (oxygens[0].toInt(2) * co2s[0].toInt(2)).toString()
     }
 
-    private data class Position(val depth: Int = 0, val horizontal: Int = 0, val aim: Int = 0) {
-        val multiplication by lazy { (depth * horizontal).toString() }
-        fun mutateDepth(amount: Int) = copy(depth = depth + amount)
-        fun mutateHorizontal(amount: Int) = copy(horizontal = horizontal + amount)
-        fun mutateAim(amount: Int) = copy(aim = aim + amount)
+    private fun filterRatings(list: MutableList<String>, index: Int, shouldEqual: Boolean) {
+        if (list.size == 1) {
+            return
+        }
+
+        val zeroes = list.count { it[index] == '0' }
+        val ones = list.count { it[index] == '1' }
+        val filter = when (zeroes.compareTo(ones)) {
+            -1 -> '1'
+            0 -> '1'
+            1 -> '0'
+            else -> throw IllegalStateException()
+        }
+
+        list.removeIf { (it[index] == filter) == shouldEqual }
     }
 }
