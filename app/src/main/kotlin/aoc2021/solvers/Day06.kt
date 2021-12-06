a66ee4bba2446e01e9c8280928d7bb6574ddbae5
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2021-12-06 11:51:05 +0100

    day 6

--- app/src/main/kotlin/aoc2021/solvers/Day06.kt
+++ app/src/main/kotlin/aoc2021/solvers/Day06.kt
@@ -1,7 +1,6 @@
 package aoc2021.solvers
 
 import aoc2021.helpers.InputLoader
-import java.util.*
 
 class Day06 : Solver {
     override fun partA() = simulate(80)
@@ -16,17 +15,15 @@ class Day06 : Solver {
     private fun simulateZero(days: Int): List<Long> {
         val values = MutableList(9) { 0L }
         values[0] = 1
-        val result = LinkedList<Long>()
 
-        for (i in 0 until days) {
-            result.add(values.sum())
+        return listOf(1L) + (1 until days).map {
             val temp = values[0]
             for (j in 0..7) {
                 values[j] = values[j + 1]
             }
             values[8] = temp
             values[6] = values[6] + temp
+            values.sum()
         }
-        return result.toList()
     }
 }
